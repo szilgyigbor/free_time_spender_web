5a5c6a54143d52aec3cb8addb7c663f04ed564c8
Gábor <szilgyigbor@gmail.com>
2023-02-13 11:24:23 +0100

    Project is created.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FreeTimeSpenderWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
