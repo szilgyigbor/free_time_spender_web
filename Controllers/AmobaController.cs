23b1b9a0dbb42f1e7ce0caa8e64211e2338582e3
Gábor <szilgyigbor@gmail.com>
2023-02-15 11:21:46 +0100

    Amoba Controller and cshtml are added.

---
+++ Controllers/AmobaController.cs
@@ -0,0 +1,13 @@
+﻿using FreeTimeSpenderWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FreeTimeSpenderWeb.Controllers
+{
+    public class AmobaController : Controller
+    {
+        public IActionResult Amoba()
+        {
+            return View();
+        }
+    }
+}
