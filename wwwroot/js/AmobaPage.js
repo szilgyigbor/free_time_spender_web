bc3c0baaa1bf3dbae527e23b84f11873bf523274
Gábor <szilgyigbor@gmail.com>
2023-02-15 12:22:56 +0100

    Gamelogic function is added to AmobaPage.js.

--- wwwroot/js/AmobaPage.js
+++ wwwroot/js/AmobaPage.js
@@ -1,5 +1,6 @@
 ﻿let amobaTable = document.querySelector("#amobaTable");
-let tableSize = 15;
+let tableSize = 20;
+let currentPlayer = "X"
 
 console.log(amobaTable);
 
@@ -10,7 +11,7 @@ for (let i = 0; i < tableSize; i++)
     for (let j = 0; j < tableSize; j++)
     {
         const cell = document.createElement('td');
-        cell.textContent = `${i},${j}`;
+        cell.textContent = ``;
         row.appendChild(cell);
     }
 
@@ -20,7 +21,51 @@ for (let i = 0; i < tableSize; i++)
 
 amobaTable.addEventListener('click', function(event) {
     let cell = event.target;
-    if (cell.tagName === 'TD') {
-      console.log(cell);
+    if (cell.tagName === 'TD') 
+    {
+      gameLogic(cell);
     }
+
   });
+
+
+function gameLogic(cell) 
+{
+    if (currentPlayer === "X")
+    {
+        
+        if (cell.textContent === "")
+        {
+            cell.textContent = "X";
+            cell.classList.add("red");
+
+            currentPlayer = "O";
+        }
+        
+        else 
+        {
+            console.log("Itt már van bábú");
+        }
+    }
+
+
+    if (currentPlayer === "O")
+    {
+        
+        if (cell.textContent === "")
+        {
+            cell.textContent = "O";
+            cell.classList.add("green");
+
+            currentPlayer = "X";
+        }
+        
+        else 
+        {
+            console.log("Itt már van bábú");
+        }
+
+    }
+    
+    
+}
