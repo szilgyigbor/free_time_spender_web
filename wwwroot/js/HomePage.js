3d3f44afd1cba8a33813a2a91327142289b52d66
Gábor <szilgyigbor@gmail.com>
2023-02-15 11:23:46 +0100

    Eventlistener is added to Amoba button in HomaPage.js.

--- wwwroot/js/HomePage.js
+++ wwwroot/js/HomePage.js
@@ -1,1 +1,7 @@
-﻿
+﻿let amobaButton = document.querySelector("#Amoba");
+
+amobaButton.addEventListener("click", navigateToAmoba);
+
+function navigateToAmoba() {
+    window.location.href = "https://localhost:44320/Amoba/Amoba";
+}
