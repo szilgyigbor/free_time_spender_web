ed4f3f3a084ee9ebcaa365241f4a57f98310c2f0
Gábor <szilgyigbor@gmail.com>
2023-02-15 14:12:39 +0100

    Winning conditions are upgraded in AmobaPage.js.

--- wwwroot/js/AmobaPage.js
+++ wwwroot/js/AmobaPage.js
@@ -1,8 +1,8 @@
 ﻿let amobaTable = document.querySelector("#amobaTable");
 let tableSize = 20;
 let currentPlayer = "X"
+let amobaString = document.querySelector("#amobaString");
 
-console.log(amobaTable);
 
 for (let i = 0; i < tableSize; i++)
 {
@@ -11,34 +11,37 @@ for (let i = 0; i < tableSize; i++)
     for (let j = 0; j < tableSize; j++)
     {
         const cell = document.createElement('td');
+        cell.setAttribute('id', `cell-${i}-${j}`);
         cell.textContent = ``;
         row.appendChild(cell);
     }
-
     amobaTable.appendChild(row);
 }
 
 
-amobaTable.addEventListener('click', function(event) {
+amobaTable.addEventListener('click', clickHandler);
+
+
+function clickHandler(event)
+{
     let cell = event.target;
     if (cell.tagName === 'TD') 
     {
-      gameLogic(cell);
+        gameLogic(cell);
     }
-
-  });
+}
 
 
 function gameLogic(cell) 
 {
     if (currentPlayer === "X")
     {
-        
+
         if (cell.textContent === "")
         {
             cell.textContent = "X";
             cell.classList.add("red");
-
+            checkWin(cell.id);
             currentPlayer = "O";
         }
         
@@ -48,15 +51,15 @@ function gameLogic(cell)
         }
     }
 
-
     if (currentPlayer === "O")
     {
         
+
         if (cell.textContent === "")
         {
             cell.textContent = "O";
             cell.classList.add("green");
-
+            checkWin(cell.id);
             currentPlayer = "X";
         }
         
@@ -64,8 +67,50 @@ function gameLogic(cell)
         {
             console.log("Itt már van bábú");
         }
-
     }
-    
-    
+
+
+    function checkWin(position)
+    {
+
+        let splitPosition = position.split("-");
+        const row  = parseInt(splitPosition[1]);
+        const collumn = parseInt(splitPosition[2]);
+        console.log(row, collumn);
+        
+        
+        for (let i = 0; i < tableSize; i++)
+        {
+            let howManyInRows = 0;
+            let howManyInCollumns = 0;
+
+            for (let j = 0; j < tableSize; j++)
+            {
+
+                if (document.querySelector(`#cell-${i}-${j}`).textContent === currentPlayer) 
+                {
+                    howManyInRows++;
+                }
+                else
+                {
+                    howManyInRows = 0;
+                }
+
+                if (document.querySelector(`#cell-${j}-${i}`).textContent === currentPlayer) 
+                {
+                    howManyInCollumns++;
+                }
+                else
+                {
+                    howManyInCollumns = 0;
+                }
+
+                if (howManyInRows === 5 || howManyInCollumns === 5)
+                {
+                    amobaString.textContent = `${currentPlayer} has won!`;
+                    amobaTable.removeEventListener('click', clickHandler);
+                }
+            }
+        }
+    }
 }
