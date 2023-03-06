5658b07ee1a1fae77bd94889e4f0d6e7ae85c44f
Gábor <szilgyigbor@gmail.com>
2023-03-06 17:30:01 +0100

    NewsController is added.

---
+++ Controllers/NewsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FreeTimeSpenderWeb.Controllers
+{
+    public class NewsController : Controller
+    {
+        public IActionResult News()
+        {
+            return View();
+        }
+    }
+}
