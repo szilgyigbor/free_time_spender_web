d72d6d80fe3a285679225dfd10f157c87640c7b6
Gábor <szilgyigbor@gmail.com>
2023-03-07 09:41:22 +0100

    NAvigateToBot function is added in HomePage.js

--- wwwroot/js/HomePage.js
+++ wwwroot/js/HomePage.js
@@ -1,15 +1,22 @@
 ﻿let amobaButton = document.querySelector("#Amoba");
 let newsButton = document.querySelector("#News");
+let talkWithBotButton = document.querySelector("#Bot");
 
 amobaButton.addEventListener("click", navigateToAmoba);
 newsButton.addEventListener("click", navigateToNews);
+talkWithBotButton.addEventListener("click", navigateToBot);
 
 
 function navigateToAmoba() {
-    window.location.href = "/Amoba/Amoba";
+    window.location.href = "/Amoba/Main";
 }
 
 
 function navigateToNews() {
-    window.location.href = "/News/News";
+    window.location.href = "/News/Main";
+}
+
+
+function navigateToBot() {
+    window.location.href = "/TalkWithBot/Main";
 }
