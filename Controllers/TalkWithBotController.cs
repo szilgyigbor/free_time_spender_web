d8d200ae79c91eee1f1468738aab2070453465d6
Gábor <szilgyigbor@gmail.com>
2023-03-07 09:37:38 +0100

    TalkWithBotController is added.

---
+++ Controllers/TalkWithBotController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FreeTimeSpenderWeb.Controllers
+{
+    public class TalkWithBotController : Controller
+    {
+        public IActionResult Main()
+        {
+            return View();
+        }
+    }
+}
