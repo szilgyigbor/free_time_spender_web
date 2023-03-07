040d346afa2c79a5627c014be730456c170dab3d
Gábor <szilgyigbor@gmail.com>
2023-03-07 09:41:59 +0100

    MapControllerRout is changed in Program.cs

--- Program.cs
+++ Program.cs
@@ -28,7 +28,7 @@ namespace FreeTimeSpenderWeb
 
             app.MapControllerRoute(
                 name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}");
+                pattern: "{controller=Home}/{action=Main}/{id?}");
 
             app.Run();
         }
