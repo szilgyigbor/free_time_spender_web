857bbacfde68dfa7043c2ad9236e74cec08c91ca
Gábor <szilgyigbor@gmail.com>
2023-03-13 17:47:48 +0100

    GetBotAnswerAsync method is added to ApiController.

--- Controllers/ApiController.cs
+++ Controllers/ApiController.cs
@@ -14,11 +14,13 @@ namespace FreeTimeSpenderWeb.Controllers
     {
         private readonly IConfiguration _configuration;
         private readonly NewsService _newsService;
+        private readonly BotService _botService;
 
-        public ApiController(IConfiguration configuration, NewsService newsService)
+        public ApiController(IConfiguration configuration, NewsService newsService, BotService botService)
         {
             _configuration = configuration;
             _newsService = newsService;
+            _botService = botService;
         }
 
 
@@ -39,6 +41,25 @@ namespace FreeTimeSpenderWeb.Controllers
             }
 
         }
+
+
+        [Route("api/getbotanswer")]
+        [HttpPost]
+        public async Task<IActionResult> GetBotAnswerAsync([FromBody] string newMessage)
+        {
+            var botApiKey = _configuration["BotApiKey"];
+            var result = await _botService.SendPostRequestAsync(botApiKey, newMessage);
+            if (result != null)
+            {
+                return Ok(result);
+            }
+            else
+            {
+                return BadRequest();
+            }
+
+        }
+
 
     }
 }
