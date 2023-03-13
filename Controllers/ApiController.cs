f24b861af5fce2efb9daba59b71bf0bd8b1b8414
Gábor <szilgyigbor@gmail.com>
2023-03-13 14:40:09 +0100

    GetNewsAsync method is adde to ApiController.

--- Controllers/ApiController.cs
+++ Controllers/ApiController.cs
@@ -1,12 +1,44 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using FreeTimeSpenderWeb.Sevices;
+using FreeTimeSpenderWeb.Models;
+using Microsoft.AspNetCore.DataProtection.KeyManagement;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Net.Http.Headers;
+
 
 namespace FreeTimeSpenderWeb.Controllers
 {
-    [Route("api/[controller]")]
+    
     [ApiController]
     public class ApiController : ControllerBase
     {
+        private readonly IConfiguration _configuration;
+        private readonly NewsService _newsService;
+
+        public ApiController(IConfiguration configuration, NewsService newsService)
+        {
+            _configuration = configuration;
+            _newsService = newsService;
+        }
+
+
+        
+        [Route("api/getnews")]
+        [HttpGet]
+        public async Task<IActionResult> GetNewsAsync()
+        {
+            var newsApiKey = _configuration["NewsApiKey"];
+            var result = await _newsService.GetNewsAsync(newsApiKey);
+            if (result != null)
+            {
+                return Ok(result);
+            }
+            else
+            {
+                return BadRequest();
+            }
+
+        }
 
     }
 }
