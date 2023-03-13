fba9acdd8c0664d327fddb03715c2379893947d3
Gábor <szilgyigbor@gmail.com>
2023-03-13 14:40:34 +0100

    NewsService is added with GetNewsAsync method.

---
+++ Sevices/NewsService.cs
@@ -0,0 +1,42 @@
+﻿using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using System.Net.Http.Json;
+using FreeTimeSpenderWeb.Models;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace FreeTimeSpenderWeb.Sevices
+{
+    public class NewsService
+    {
+        private readonly IHttpClientFactory _clientFactory;
+
+        public NewsService(IHttpClientFactory clientFactory)
+        {
+            _clientFactory = clientFactory;
+        }
+
+        public async Task<string> GetNewsAsync(string apiKey)
+        {
+            var client = _clientFactory.CreateClient();
+            var country = "hu";
+
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", apiKey);
+            client.DefaultRequestHeaders.Add("User-Agent", "Free-Time-Spender");
+
+            var response = await client.GetAsync($"https://newsapi.org/v2/top-headlines?country={country}&apiKey={apiKey}");
+
+            if (response.IsSuccessStatusCode)
+            {
+                return await response.Content.ReadAsStringAsync();
+            }
+            else
+            {
+                var errorMessage = await response.Content.ReadAsStringAsync();
+                return null;
+            }
+        }
+    }
+}
