61dc3c0187040c8adce8a48260b6f2fe2f45668b
Gábor <szilgyigbor@gmail.com>
2023-03-22 20:53:56 +0100

    WeatherService is added with GetWeatherAsync method.

---
+++ Sevices/WeatherService.cs
@@ -0,0 +1,32 @@
+﻿namespace FreeTimeSpenderWeb.Sevices
+{
+    public class WeatherService
+    {
+        private readonly HttpClient _httpClient;
+
+        public WeatherService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<string> GetWeatherAsync(string apiKey, string location)
+        {
+            var request = new HttpRequestMessage(HttpMethod.Get, $"http://api.weatherapi.com/v1/current.json?key={apiKey}&q={location}&aqi=no");
+
+            request.Headers.Add("Authorization", "Bearer " + apiKey);
+            request.Headers.Add("User-Agent", "Free-Time-Spender");
+
+            var response = await _httpClient.SendAsync(request);
+
+            if (response.IsSuccessStatusCode)
+            {
+                return await response.Content.ReadAsStringAsync();
+            }
+            else
+            {
+                var errorMessage = await response.Content.ReadAsStringAsync();
+                return null;
+            }
+        }
+    }
+}
