a50a7181483de168064a890584b6bbd8a734c5eb
Gábor <szilgyigbor@gmail.com>
2023-04-05 20:49:40 +0200

    WeatherService is refactored.

--- Sevices/WeatherService.cs
+++ Sevices/WeatherService.cs
@@ -11,22 +11,17 @@
 
         public async Task<string> GetWeatherAsync(string apiKey, string location)
         {
-            var request = new HttpRequestMessage(HttpMethod.Get, $"http://api.weatherapi.com/v1/current.json?key={apiKey}&q={location}&aqi=no");
-
+            /*var request = new HttpRequestMessage(HttpMethod.Get, $"http://api.weatherapi.com/v1/current.json?key={apiKey}&q={location}&aqi=no");
+            
             request.Headers.Add("Authorization", "Bearer " + apiKey);
             request.Headers.Add("User-Agent", "Free-Time-Spender");
 
-            var response = await _httpClient.SendAsync(request);
+            var response = await _httpClient.SendAsync(request);*/
 
-            if (response.IsSuccessStatusCode)
-            {
-                return await response.Content.ReadAsStringAsync();
-            }
-            else
-            {
-                var errorMessage = await response.Content.ReadAsStringAsync();
-                return null;
-            }
+            var response = await _httpClient.GetAsync($"http://api.weatherapi.com/v1/current.json?key={apiKey}&q={location}&aqi=no");
+
+            return await response.Content.ReadAsStringAsync();
+            
         }
     }
 }
