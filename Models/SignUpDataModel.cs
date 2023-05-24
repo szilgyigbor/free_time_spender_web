98cd26e6e054d612a755daca06caf67308ae94ec
Gábor <szilgyigbor@gmail.com>
2023-05-24 11:43:08 +0200

    Id property is added to the SignUpDataModel.

--- Models/SignUpDataModel.cs
+++ Models/SignUpDataModel.cs
@@ -1,9 +1,17 @@
-﻿namespace FreeTimeSpenderWeb.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FreeTimeSpenderWeb.Models
 {
     public class SignUpDataModel
     {
-        public string Username { get; set; }
-        public string Email { get; set; }
-        public string Password { get; set; }
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string? Username { get; set; }
+        [Required]
+        public string? Email { get; set; }
+        [Required]
+        public string? Password { get; set; }
     }
 }
