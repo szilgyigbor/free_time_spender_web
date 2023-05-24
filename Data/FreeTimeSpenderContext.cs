21355c107368da4b4a74066846121e9c5c5f4986
Gábor <szilgyigbor@gmail.com>
2023-05-24 11:41:39 +0200

    FreeTimeSpenderContext.cs is added.

---
+++ Data/FreeTimeSpenderContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using FreeTimeSpenderWeb.Models;
+
+namespace FreeTimeSpenderWeb.Data
+{
+    public class FreeTimeSpenderContext : DbContext
+    {
+        public DbSet<SignUpDataModel> SignUpDatas { get; set; }
+
+        public FreeTimeSpenderContext(DbContextOptions<FreeTimeSpenderContext> options) : base(options)
+        {
+        }
+    }
+}
