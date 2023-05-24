d48d0517117600dd31dd1f71ba11fbbc51ad5ec3
Gábor <szilgyigbor@gmail.com>
2023-05-24 09:31:08 +0200

    UserApiController is added.

---
+++ Controllers/UserApiController.cs
@@ -0,0 +1,18 @@
+﻿using FreeTimeSpenderWeb.Sevices;
+using Microsoft.AspNetCore.Mvc;
+using FreeTimeSpenderWeb.Models;
+
+namespace FreeTimeSpenderWeb.Controllers
+{
+    [Route("api/user")]
+    [ApiController]
+    public class UserApiController : ControllerBase
+    {
+        [Route("signup")]
+        [HttpPost]
+        public async Task<SignUpDataModel> SignUp([FromBody] SignUpDataModel signUpData)
+        {
+            return signUpData;
+        }
+    }
+}
