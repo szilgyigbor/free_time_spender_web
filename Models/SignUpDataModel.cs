4eef88fcfbd8283929323f030afe212923b61b8a
Gábor <szilgyigbor@gmail.com>
2023-05-24 09:30:52 +0200

    SignUpDataModel is added.

---
+++ Models/SignUpDataModel.cs
@@ -0,0 +1,9 @@
+﻿namespace FreeTimeSpenderWeb.Models
+{
+    public class SignUpDataModel
+    {
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
