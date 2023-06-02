c7aa82904d9c26cfbb12c5927230b772508461cc
Gábor <szilgyigbor@gmail.com>
2023-06-02 15:12:10 +0200

    Removed unused MVC components and switch to API-only architecture.

--- Program.cs
+++ Program.cs
@@ -1,6 +1,5 @@
 using FreeTimeSpenderWeb.Services;
 using Microsoft.EntityFrameworkCore;
-using FreeTimeSpenderWeb.Models;
 using FreeTimeSpenderWeb.Data;
 using FreeTimeSpenderWeb.Services.Interfaces;
 
@@ -11,9 +10,9 @@ namespace FreeTimeSpenderWeb
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
-            
+
             // Add services to the container.
-            builder.Services.AddControllersWithViews();
+            builder.Services.AddControllers();
             builder.Services.AddHttpClient();
 
             builder.Services.AddDbContext<FreeTimeSpenderContext>(options => options.UseSqlServer(
@@ -24,31 +23,22 @@ namespace FreeTimeSpenderWeb
             builder.Services.AddTransient<IFlickrService, FlickrService>();
             builder.Services.AddTransient<IWeatherService, WeatherService>();
 
-            /*builder.Services.AddTransient<NewsService>();
-            builder.Services.AddTransient<BotService>();
-            builder.Services.AddTransient<WeatherService>();
-            builder.Services.AddTransient<FlickrService>();*/
-
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
             if (!app.Environment.IsDevelopment())
             {
-                app.UseExceptionHandler("/Home/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
             app.UseHttpsRedirection();
-            app.UseStaticFiles();
 
             app.UseRouting();
 
             app.UseAuthorization();
 
-            app.MapControllerRoute(
-                name: "default",
-                pattern: "{controller=Home}/{action=Main}/{id?}");
+            app.MapControllers();
 
             app.Run();
         }
