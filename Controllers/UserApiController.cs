676a8593d4270d855cb6dc65da63af515495b81f
Gábor <szilgyigbor@gmail.com>
2023-06-08 12:10:35 +0200

    Constructor and a private UserService field are added to the UserApiController.

--- Controllers/UserApiController.cs
+++ Controllers/UserApiController.cs
@@ -1,6 +1,11 @@
 ﻿using FreeTimeSpenderWeb.Services;
 using Microsoft.AspNetCore.Mvc;
 using FreeTimeSpenderWeb.Models;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+
 
 namespace FreeTimeSpenderWeb.Controllers
 {
@@ -8,6 +13,15 @@ namespace FreeTimeSpenderWeb.Controllers
     [ApiController]
     public class UserApiController : ControllerBase
     {
+        private readonly UserService _userService;
+
+
+        public UserApiController(UserService userService)
+        {
+            _userService = userService;
+        }
+
+
         [Route("signup")]
         [HttpPost]
         public async Task<UserDataModel> SignUp([FromBody] UserDataModel signUpData)
