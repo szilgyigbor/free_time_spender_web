40b9ba87e9e66dcb2c1fd2b6c2d3df17a8b9e40a
Gábor <szilgyigbor@gmail.com>
2023-06-08 09:55:53 +0200

    AccountService is added with RegisterUser method.

---
+++ Services/AccountService.cs
@@ -0,0 +1,40 @@
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using FreeTimeSpenderWeb.Data;
+using FreeTimeSpenderWeb.Models;
+using FreeTimeSpenderWeb.Services.Interfaces;
+
+
+
+namespace FreeTimeSpenderWeb.Services
+{
+    public class AccountService
+    {
+        private readonly FreeTimeSpenderContext _context;
+        private PasswordHasher<UserDataModel> PasswordHasher { get; }
+
+        public AccountService(FreeTimeSpenderContext context)
+        {
+            _context = context;
+            PasswordHasher = new PasswordHasher<UserDataModel>();
+        }
+
+        public async Task RegisterUser(UserDataModel registerData)
+        {
+            var user = new UserDataModel()
+            {
+                Email = registerData.Email,
+                Username = registerData.Email,
+                
+            };
+
+            user.Password = PasswordHasher.HashPassword(user, registerData.Password);
+
+
+            _context.SignUpDatas.Add(user);
+            await _context.SaveChangesAsync();
+
+        }
+    }
+}
