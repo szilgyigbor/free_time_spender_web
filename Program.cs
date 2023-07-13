4b7a4f2e64597e3a0ef2edcac7a779eba88496ce
Gábor <szilgyigbor@gmail.com>
2023-07-13 12:20:02 +0200

    Replace SQLPassword with environment variable

--- Program.cs
+++ Program.cs
@@ -39,8 +39,9 @@ namespace FreeTimeSpenderWeb
             builder.Services.AddHttpClient();
 
             builder.Services.AddDbContext<FreeTimeSpenderContext>(options => options.UseSqlServer(
-                builder.Configuration.GetConnectionString("DefaultConnection")?.Replace("{SQLPassword}", Environment.GetEnvironmentVariable("SQLPassword")
-            )));
+                builder.Configuration.GetConnectionString("DefaultConnection")
+                ?.Replace("{SQLPassword}", Environment.GetEnvironmentVariable("SQLPassword"))
+            ));
             builder.Services.AddTransient<INewsService, NewsService>();
             builder.Services.AddTransient<IBotService, BotService>();
             builder.Services.AddTransient<IFlickrService, FlickrService>();
