a6df89efeea847d2e7e8fe94e4b3cd6d7523a208
Gábor <szilgyigbor@gmail.com>
2023-07-13 12:21:45 +0200

    Add Cors policy.

--- Program.cs
+++ Program.cs
@@ -69,6 +69,11 @@ namespace FreeTimeSpenderWeb
 
             app.UseRouting();
 
+            app.UseCors(policy => policy
+                .WithOrigins("https://www.digitalisjatszoter.hu")
+                .AllowAnyMethod()
+                .AllowAnyHeader());
+
             app.UseAuthorization();
 
             app.MapControllers();
