b4ac7cb7215741fdd329a41d9c5f062a028bc020
Gábor <szilgyigbor@gmail.com>
2023-07-14 16:23:25 +0200

    Add(GameHub.cs): Add GameHub class with MoveCharacter method.

---
+++ Hubs/GameHub.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace FreeTimeSpenderWeb.Hubs
+{
+    public class GameHub : Hub
+    {
+        public async Task MoveCharacter(string username, int newX, int newY)
+        {
+
+            await Clients.Others.SendAsync("CharacterMoved", username, newX, newY);
+        }
+    }
+}
