5028d54c60ca051bf1caf44d2f1f8d8f60fd3dec
Gábor <szilgyigbor@gmail.com>
2023-07-20 15:44:19 +0200

    Create MoveCharacter method in GameHub class

--- Hubs/GameHub.cs
+++ Hubs/GameHub.cs
@@ -1,13 +1,39 @@
 ﻿using Microsoft.AspNetCore.SignalR;
+using Microsoft.AspNetCore.Authorization;
+using FreeTimeSpenderWeb.Models;
+using System.Collections.Generic;
 
 namespace FreeTimeSpenderWeb.Hubs
 {
     public class GameHub : Hub
     {
+
+        private static List<PlayerModel> Players = new List<PlayerModel>();
+
+
         public async Task MoveCharacter(string username, int newX, int newY)
         {
+            PlayerModel player = Players.Find(p => p.Name == username)!;
 
-            await Clients.Others.SendAsync("CharacterMoved", username, newX, newY);
+            if (player != null)
+            {
+                player.PositionX = newX;
+                player.PositionY = newY;
+            }
+
+            else { 
+                player = new PlayerModel
+                {
+                    Name = username,
+                    PositionX = newX,
+                    PositionY = newY,
+                    Health = 100
+                };
+
+                Players.Add(player);
+            }
+
+            await Clients.All.SendAsync("PlayersMoved", Players);
         }
     }
 }
