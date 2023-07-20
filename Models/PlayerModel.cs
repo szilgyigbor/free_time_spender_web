8409e752a8ca78a732b94e9cfe7d8c74e597a03d
Gábor <szilgyigbor@gmail.com>
2023-07-20 15:44:39 +0200

    Add PlayerModel class

---
+++ Models/PlayerModel.cs
@@ -0,0 +1,10 @@
+﻿namespace FreeTimeSpenderWeb.Models
+{
+    public class PlayerModel
+    {
+        public string? Name { get; set; }
+        public int PositionX { get; set; }
+        public int PositionY { get; set; }
+        public int Health { get; set; }
+    }
+}
