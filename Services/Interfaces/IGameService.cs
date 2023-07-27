7180c73ef83696313449e4c278151fa6c34e6556
Gábor <szilgyigbor@gmail.com>
2023-07-27 11:38:16 +0200

    Add IGameService interface

---
+++ Services/Interfaces/IGameService.cs
@@ -0,0 +1,14 @@
+﻿using FreeTimeSpenderWeb.Models;
+
+namespace FreeTimeSpenderWeb.Services.Interfaces
+{
+    public interface IGameService
+    {
+        List<PlayerModel> GetPlayers();
+
+        List<PlayerModel> UpdatePlayer(string username, int newX, int newY);
+
+        Task UpdateBot();
+
+    }
+}
