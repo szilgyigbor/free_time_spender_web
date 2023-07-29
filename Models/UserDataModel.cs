bc65aeb01077f406ceaac6338c22e8092abf437d
Gábor <szilgyigbor@gmail.com>
2023-07-29 11:18:48 +0200

    Change column type to "text" in UserDataModel for PostgreSQL

--- Models/UserDataModel.cs
+++ Models/UserDataModel.cs
@@ -1,4 +1,6 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace FreeTimeSpenderWeb.Models
 {
@@ -8,9 +10,13 @@ namespace FreeTimeSpenderWeb.Models
         public int Id { get; set; }
 
         [Required]
+        [Column(TypeName = "text")]
         public string? Username { get; set; }
 
+        [Column(TypeName = "text")]
         public string? Email { get; set; }
+
+        [Column(TypeName = "text")]
         [Required]
         public string? Password { get; set; }
     }
