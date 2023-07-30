1ef6e3d137ee657f40b4f3e0d0c68067b2c127ab
Gábor <szilgyigbor@gmail.com>
2023-07-30 10:44:18 +0200

    Add MessageDatas DbSet to FreeTimeSpenderContext

--- Data/FreeTimeSpenderContext.cs
+++ Data/FreeTimeSpenderContext.cs
@@ -6,6 +6,7 @@ namespace FreeTimeSpenderWeb.Data
     public class FreeTimeSpenderContext : DbContext
     {
         public DbSet<UserDataModel> SignUpDatas { get; set; }
+        public DbSet<MessageDataModel> MessageDatas { get; set; }
 
         public FreeTimeSpenderContext(DbContextOptions<FreeTimeSpenderContext> options) : base(options)
         {
