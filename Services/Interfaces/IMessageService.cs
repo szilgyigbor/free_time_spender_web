91568ee94e3fe7161423f63ae673948bafff5b14
Gábor <szilgyigbor@gmail.com>
2023-07-30 10:57:26 +0200

    Upgrade(IMessageService): - Add Getmessages method. - Add AddMessage method.

--- Services/Interfaces/IMessageService.cs
+++ Services/Interfaces/IMessageService.cs
@@ -1,6 +1,11 @@
-﻿namespace FreeTimeSpenderWeb.Services.Interfaces
+﻿using FreeTimeSpenderWeb.Models;
+
+namespace FreeTimeSpenderWeb.Services.Interfaces
 {
     public interface IMessageService
     {
+        Task<IEnumerable<MessageDataModel>> GetMessages();
+
+        Task AddMessage(MessageDataModel message);
     }
 }
