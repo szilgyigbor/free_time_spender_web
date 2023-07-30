0c5bb44900a0667cf0bee0759c9b6b258b3ad3a7
Gábor <szilgyigbor@gmail.com>
2023-07-30 10:44:33 +0200

    Add MessageDataModel

---
+++ Models/MessageDataModel.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace FreeTimeSpenderWeb.Models
+{
+    public class MessageDataModel
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [Column(TypeName = "text")]
+        [StringLength(500)]
+        public string? Message { get; set; }
+
+        [Required]
+        [Column(TypeName = "text")]
+        [StringLength(50)]
+        public string? Username { get; set; }
+
+        public DateTime PostedAt { get; set; }
+    }
+}
