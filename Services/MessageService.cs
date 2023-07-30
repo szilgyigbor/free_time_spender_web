7248ba7893c8cc1108cf26108361df8c7b00f712
Gábor <szilgyigbor@gmail.com>
2023-07-30 10:44:51 +0200

    Add MessageService

---
+++ Services/MessageService.cs
@@ -0,0 +1,17 @@
+﻿using FreeTimeSpenderWeb.Data;
+using FreeTimeSpenderWeb.Services.Interfaces;
+
+namespace FreeTimeSpenderWeb.Services
+{
+    public class MessageService : IMessageService
+    {
+        private readonly FreeTimeSpenderContext _context;
+
+        public MessageService(FreeTimeSpenderContext context)
+        {
+            _context = context;
+        }
+
+
+    }
+}
