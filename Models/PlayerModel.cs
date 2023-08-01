1887c518dfaca7c3a3f9d49ab55fe13cfa98b36c
Gábor <szilgyigbor@gmail.com>
2023-08-01 14:05:15 +0200

    Add IsReversed property to PlayerModel.

--- Models/PlayerModel.cs
+++ Models/PlayerModel.cs
@@ -6,5 +6,6 @@
         public int PositionX { get; set; }
         public int PositionY { get; set; }
         public int Health { get; set; }
+        public bool IsReversed { get; set; }
     }
 }
