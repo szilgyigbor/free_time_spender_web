d6b0a41775c812080ba519f60e26d9225464e9c9
Gábor <szilgyigbor@gmail.com>
2023-08-02 12:53:36 +0200

    Add Shoter property to PlayerModel.

--- Models/PlayerModel.cs
+++ Models/PlayerModel.cs
@@ -7,5 +7,6 @@
         public int PositionY { get; set; }
         public int Health { get; set; }
         public bool IsReversed { get; set; }
+        public string? Shooter { get; set; }
     }
 }
