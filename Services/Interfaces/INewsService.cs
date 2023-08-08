8530b6fff4a50dfefa4d4d8e365191a957af9ced
Gábor <szilgyigbor@gmail.com>
2023-08-08 22:43:57 +0200

    Add GetOneNewsAsync method.

--- Services/Interfaces/INewsService.cs
+++ Services/Interfaces/INewsService.cs
@@ -1,7 +1,11 @@
-﻿namespace FreeTimeSpenderWeb.Services.Interfaces
+﻿using FreeTimeSpenderWeb.Models;
+
+namespace FreeTimeSpenderWeb.Services.Interfaces
 {
     public interface INewsService
     {
         Task<string> GetNewsAsync(string apiKey);
+
+        Task<ArticleModel> GetOneNewsAsync(string apiKey);
     }
 }
