512f2ea1561385b571ad050f3adf7b117dbbc952
Gábor <szilgyigbor@gmail.com>
2023-08-16 11:33:49 +0200

    Add ResultApiController: - Add GetResults method. - Add AddResult method.

---
+++ Controllers/ResultApiController.cs
@@ -0,0 +1,42 @@
+﻿using FreeTimeSpenderWeb.Models;
+using FreeTimeSpenderWeb.Services.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FreeTimeSpenderWeb.Controllers
+{
+    [ApiController]
+    public class ResultApiController : ControllerBase
+    {
+        private readonly ISortingGameService _sortingGameService;
+
+        public ResultApiController(ISortingGameService sortingGameService)
+        {
+            _sortingGameService = sortingGameService;
+        }
+
+        [Route("api/getsortingresults")]
+        [HttpGet]
+        public async Task<IActionResult> GetResults()
+        {
+            var result = await _sortingGameService.GetResults();
+            if (result != null)
+            {
+                return Ok(result);
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+
+        [Authorize]
+        [Route("api/addsortingresult")]
+        [HttpPost]
+        public async Task<IActionResult> AddResult([FromBody] SortingGameData sortingGameData)
+        {
+            await _sortingGameService.AddResult(sortingGameData);
+            return Ok();
+        }
+    }
+}
