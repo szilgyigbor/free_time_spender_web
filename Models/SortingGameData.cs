ac85bd3ede0f5235e73a980bf9d4489a776d76a3
Gábor <szilgyigbor@gmail.com>
2023-08-16 11:08:13 +0200

    Add SortingGameData model.

---
+++ Models/SortingGameData.cs
@@ -0,0 +1,9 @@
+﻿namespace FreeTimeSpenderWeb.Models
+{
+    public class SortingGameData
+    {
+        public int Id { get; set; }
+        public string? Username { get; set; }
+        public int TimeInSeconds { get; set; }
+    }
+}
