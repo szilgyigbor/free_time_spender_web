cd1d6a4db021e719cd8ba5be2110c644c420dece
Gábor <szilgyigbor@gmail.com>
2023-08-16 11:30:54 +0200

    Upgrade(SortingGamingService): - Add db context. - Add GetSortingGameDatas method. - Add AddResult method.

--- Services/SortingGameService.cs
+++ Services/SortingGameService.cs
@@ -1,8 +1,28 @@
-﻿using FreeTimeSpenderWeb.Services.Interfaces;
+﻿using FreeTimeSpenderWeb.Data;
+using FreeTimeSpenderWeb.Models;
+using FreeTimeSpenderWeb.Services.Interfaces;
+using Microsoft.EntityFrameworkCore;
 
 namespace FreeTimeSpenderWeb.Services
 {
     public class SortingGameService : ISortingGameService
     {
+        private readonly FreeTimeSpenderContext _context;
+
+        public SortingGameService(FreeTimeSpenderContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<SortingGameData>> GetSortingGameDatas()
+        {
+            return await _context.SortingGameDatas.ToListAsync();
+        }
+
+        public async Task AddResult(SortingGameData sortingGameData)
+        {
+            await _context.SortingGameDatas.AddAsync(sortingGameData);
+            await _context.SaveChangesAsync();
+        }
     }
 }
