a883a8e365ea5d10fc8b8cdea5a77f69f8fb93b3
Gábor <szilgyigbor@gmail.com>
2023-09-01 11:49:35 +0200

    Add PageNewsApiController.

---
+++ Controllers/PageNewsApiController.cs
@@ -0,0 +1,44 @@
+﻿using FreeTimeSpenderWeb.Models;
+using FreeTimeSpenderWeb.Services.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace FreeTimeSpenderWeb.Controllers
+{
+    [Route("api/pagenews")]
+    [ApiController]
+    public class PageNewsApiController : ControllerBase
+    {
+        private readonly IPageNewsService _pageNewsService;
+
+        public PageNewsApiController(IPageNewsService pageNewsService)
+        {
+            _pageNewsService = pageNewsService;
+        }
+
+        [Route("getpagenews")]
+        [HttpGet]
+        public async Task<IActionResult> GetPageNews()
+        {
+            var result = await _pageNewsService.GetPageNews();
+            if (result != null)
+            {
+                return Ok(result);
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+
+        [Authorize]
+        [Route("addpagenews")]
+        [HttpPost]
+        public async Task<IActionResult> AddPageNews([FromBody] PageNewsData pageNews)
+        {
+            await _pageNewsService.AddPageNews(pageNews);
+            return Ok();
+        }
+    }
+}
