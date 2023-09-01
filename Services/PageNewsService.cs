75c1a89e03ab533912b16c2f1b156b9c2cc610b0
Gábor <szilgyigbor@gmail.com>
2023-09-01 11:48:56 +0200

    Add PageNewsService: - Add GetPageNews method. - Add AddPageNews method.

---
+++ Services/PageNewsService.cs
@@ -0,0 +1,28 @@
+﻿using FreeTimeSpenderWeb.Data;
+using FreeTimeSpenderWeb.Services.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using FreeTimeSpenderWeb.Models;
+
+namespace FreeTimeSpenderWeb.Services
+{
+    public class PageNewsService : IPageNewsService
+    {
+        private readonly FreeTimeSpenderContext _context;
+
+        public PageNewsService(FreeTimeSpenderContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<PageNewsData>> GetPageNews()
+        {
+            return await _context.PageNewsDatas.ToListAsync();
+        }
+
+        public async Task AddPageNews(PageNewsData pageNews)
+        {
+            await _context.PageNewsDatas.AddAsync(pageNews);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
