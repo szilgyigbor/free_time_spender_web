96d555d951ccfd0873ec48281a6f7b108be800cd
Gábor <szilgyigbor@gmail.com>
2023-09-01 10:48:45 +0200

    Add ChangeTableName migration

---
+++ Migrations/20230901084035_ChangeTableNames.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace FreeTimeSpenderWeb.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeTableNames : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameTable(
+                name: "MessageDatas",
+                newName: "OpinionDatas");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameTable(
+                name: "OpinionDatas",
+                newName: "MessageDatas");
+        }
+    }
+}
