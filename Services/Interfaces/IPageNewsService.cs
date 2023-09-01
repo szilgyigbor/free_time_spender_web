6d05c314bcadf914e734a2f4a50a754bb87e4b71
Gábor <szilgyigbor@gmail.com>
2023-09-01 11:49:15 +0200

    Add IPageNewsService interface.

---
+++ Services/Interfaces/IPageNewsService.cs
@@ -0,0 +1,11 @@
+﻿using FreeTimeSpenderWeb.Models;
+
+namespace FreeTimeSpenderWeb.Services.Interfaces
+{
+    public interface IPageNewsService
+    {
+        Task<IEnumerable<PageNewsData>> GetPageNews();
+
+        Task AddPageNews(PageNewsData pageNews);
+    }
+}
