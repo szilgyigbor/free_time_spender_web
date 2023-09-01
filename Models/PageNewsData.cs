ef45731eea2c35fca022b2ed33a671630aec111e
Gábor <szilgyigbor@gmail.com>
2023-09-01 11:46:44 +0200

    Add PageNewsData model.

---
+++ Models/PageNewsData.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace FreeTimeSpenderWeb.Models
+{
+    public class PageNewsData
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [Column(TypeName = "text")]
+        [StringLength(500)]
+        public string? Message { get; set; }
+
+
+        public DateTime PostedAt { get; set; }
+    }
+}
