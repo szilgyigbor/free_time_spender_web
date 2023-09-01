350ac64e0ad218f21bdf9d2d51075ff87ef1bb2c
Gábor <szilgyigbor@gmail.com>
2023-09-01 11:48:01 +0200

    Add "AddPageNewsDataModel" migration.

---
+++ Migrations/20230901094547_AddPageNewsData.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace FreeTimeSpenderWeb.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPageNewsData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "PageNewsDatas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Message = table.Column<string>(type: "text", maxLength: 500, nullable: false),
+                    PostedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PageNewsDatas", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PageNewsDatas");
+        }
+    }
+}
