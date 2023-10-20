32f0b9f9ced27a539b3c5a53231f69bbf180fb1f
Gábor <szilgyigbor@gmail.com>
2023-10-20 11:51:21 +0200

    Add VisitData model.

---
+++ Models/VisitData.cs
@@ -0,0 +1,9 @@
+﻿namespace FreeTimeSpenderWeb.Models
+{
+    public class VisitData
+    {
+        public int Id { get; set; }
+        public int AllVisits { get; set; }
+        public DateTime LastVisit { get; set; }
+    }
+}
