2d6fbdb4e2f37c139f8ecfa802c14630c186f91f
Gábor <szilgyigbor@gmail.com>
2023-10-20 11:52:39 +0200

    Add VisitorManagementService: - Add GetVisits method. - Add RaiseVisitNumber method.

---
+++ Services/VisitorManagementService.cs
@@ -0,0 +1,41 @@
+﻿using FreeTimeSpenderWeb.Data;
+using FreeTimeSpenderWeb.Services.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using FreeTimeSpenderWeb.Models;
+
+namespace FreeTimeSpenderWeb.Services
+{
+    public class VisitorManagementService : IVisitorManagementService
+    {
+        private readonly FreeTimeSpenderContext _context;
+
+        public VisitorManagementService(FreeTimeSpenderContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<VisitData> GetVisits()
+        {
+            return await _context.VisitDatas.FirstOrDefaultAsync();
+        }
+
+        public async Task<VisitData> RaiseVisitNumber()
+        {
+            VisitData visitData = await _context.VisitDatas.FirstOrDefaultAsync();
+
+            if (visitData == null)
+            {
+                visitData = new VisitData { AllVisits = 0, LastVisit = DateTime.UtcNow };
+                _context.VisitDatas.Add(visitData);
+            }
+
+            visitData.AllVisits++;
+            visitData.LastVisit = DateTime.UtcNow;
+
+            await _context.SaveChangesAsync();
+
+            return visitData;
+        }
+
+    }
+}
