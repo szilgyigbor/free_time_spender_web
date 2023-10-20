188ca9f9f58bd8ec01d8df9e4a7743d6da42980c
Gábor <szilgyigbor@gmail.com>
2023-10-20 11:53:02 +0200

    Add IVisitorManagementService interface: - Add GetVisits method. - Add RaiseVisitNumber method.

---
+++ Services/Interfaces/IVisitorManagementService.cs
@@ -0,0 +1,12 @@
+﻿using FreeTimeSpenderWeb.Models;
+
+
+namespace FreeTimeSpenderWeb.Services.Interfaces
+{
+    public interface IVisitorManagementService
+    {
+        Task<VisitData> GetVisits();
+
+        Task<VisitData> RaiseVisitNumber();
+    }
+}
