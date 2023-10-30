87a284317b7369c7618452a7d3b05a871256cf12
Gábor <szilgyigbor@gmail.com>
2023-10-30 10:20:40 +0100

    Add ForumController.cs

---
+++ Controllers/ForumController.cs
@@ -0,0 +1,48 @@
+﻿using FreeTimeSpenderWeb.Models;
+using FreeTimeSpenderWeb.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FreeTimeSpenderWeb.Controllers
+{
+    [Route("forum")]
+    [ApiController]
+    public class ForumController : ControllerBase
+    {
+        private readonly IForumService _forumService;
+
+        public ForumController(IForumService forumService)
+        {
+            _forumService = forumService;
+        }
+
+        [Route("getposts")]
+        [HttpGet]
+        public async Task<IActionResult> GetPosts()
+        {
+            var result = await _forumService.GetPosts();
+            if (result != null)
+            {
+                return Ok(result);
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+
+        [Route("getpost/{id}")]
+        [HttpGet]
+        public async Task<IActionResult> GetPostById(int id)
+        {
+            var result = await _forumService.GetPostById(id);
+            if (result != null)
+            {
+                return Ok(result);
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+    }
+}
