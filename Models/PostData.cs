374ec6debd444624c43483d3648abe735aa48375
Gábor <szilgyigbor@gmail.com>
2023-10-30 09:30:11 +0100

    Add PostData.cs

---
+++ Models/PostData.cs
@@ -0,0 +1,12 @@
+﻿namespace FreeTimeSpenderWeb.Models
+{
+    public class PostData
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public string? Content { get; set; }
+        public int LikeCount { get; set; }
+        public int DislikeCount { get; set; }
+        public List<CommentData>? Comments { get; set; }
+    }
+}
