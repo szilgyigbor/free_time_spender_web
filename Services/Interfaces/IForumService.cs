109c858e5a4d1d35f1d2532daa4cd2f985b534fd
Gábor <szilgyigbor@gmail.com>
2023-10-30 10:03:21 +0100

    Add IForumService interface

---
+++ Services/Interfaces/IForumService.cs
@@ -0,0 +1,20 @@
+﻿using FreeTimeSpenderWeb.Data;
+using FreeTimeSpenderWeb.Models;
+
+namespace FreeTimeSpenderWeb.Services.Interfaces
+{
+    public interface IForumService
+    {
+        Task<IEnumerable<PostData>> GetPosts();
+
+        Task<IEnumerable<PostData>> GetPostById(int postId);
+
+        Task AddPost(PostData post);
+
+        Task AddComment(CommentData comment);
+
+        Task DeletePostById(int postId);
+
+        Task DeleteCommentById(int commentId);
+    }
+}
