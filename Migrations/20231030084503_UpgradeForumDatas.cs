4b7b0a4086bbbf37490afbc7cf49b174054c3346
Gábor <szilgyigbor@gmail.com>
2023-10-30 09:46:11 +0100

    Add "UpgradeForumDatas" migration script.

---
+++ Migrations/20231030084503_UpgradeForumDatas.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FreeTimeSpenderWeb.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpgradeForumDatas : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Author",
+                table: "Posts",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Author",
+                table: "Comments",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Author",
+                table: "Posts");
+
+            migrationBuilder.DropColumn(
+                name: "Author",
+                table: "Comments");
+        }
+    }
+}
